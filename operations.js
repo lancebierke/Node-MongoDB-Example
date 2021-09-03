fa14dc2f8e6b04488fba1323105f6e9dac8faf1b
Lance Bierke <lancebierke@gmail.com>
2021-09-03 10:39:57 -0700

    Node Callback Hell and Promises

--- operations.js
+++ operations.js
@@ -1,21 +1,19 @@
-const assert = require("assert").strict;
-
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
+  return coll.find({}).toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
+  return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
+  return coll.updateOne(document, { $set: update }, null);
 };
